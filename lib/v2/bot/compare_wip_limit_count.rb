a857adf92d75fab57d054e2bfb64df8820c655e9
Felipe Guzmán Sierra <97761783+FelipeGuzmanSierra@users.noreply.github.com>
2024-05-10 14:08:24 -0500

    Solve #33 - Refactor WIP limit Use Case (#40)
    * Add fetch domains wip counts from notion bot
    
    * Add fetch domains wip limit from notion bot
    
    * Add compare wip limit count bot
    
    * Add format wip limit exceeded bot
    
    * Improve test descriptions

---
+++ lib/v2/bot/compare_wip_limit_count.rb
@@ -0,0 +1,89 @@
+# frozen_string_literal: true
+
+require_relative "./base"
+require_relative "../read/postgres"
+require_relative "../write/postgres"
+
+module Bot
+  ##
+  # The Bot::CompareWipLimitCount class serves as a bot implementation to read domains wip limits and
+  # counts from a PostgresDB database, compare the values to find exceeded counts, and write them on
+  # a PostgresDB table with a specific format.
+  #
+  # <br>
+  # <b>Example</b>
+  #
+  #   options = {
+  #     read_options: {
+  #       connection: {
+  #         host: "localhost",
+  #         port: 5432,
+  #         dbname: "bas",
+  #         user: "postgres",
+  #         password: "postgres"
+  #       },
+  #       db_table: "use_cases",
+  #       bot_name: "FetchDomainsWipLimitFromNotion"
+  #     },
+  #     write_options: {
+  #       connection: {
+  #         host: "localhost",
+  #         port: 5432,
+  #         dbname: "bas",
+  #         user: "postgres",
+  #         password: "postgres"
+  #       },
+  #       db_table: "use_cases",
+  #       bot_name: "CompareWipLimitCount"
+  #     }
+  #   }
+  #
+  #   bot = Bot::CompareWipLimitCount.new(options)
+  #   bot.execute
+  #
+  class CompareWipLimitCount < Bot::Base
+    # read function to execute the PostgresDB Read component
+    #
+    def read
+      reader = Read::Postgres.new(read_options)
+
+      reader.execute
+    end
+
+    # Process function to compare the domains wip counts and limits
+    #
+    def process(read_response)
+      return { success: { exceeded_domain_count: {} } } if unprocessable_response(read_response.data)
+
+      domains_limits = read_response.data["domains_limits"]
+      domain_wip_count = read_response.data["domain_wip_count"]
+
+      exceeded_domain_count = exceedded_counts(domains_limits, domain_wip_count)
+
+      { success: { exceeded_domain_count: } }
+    end
+
+    # Write function to execute the PostgresDB write component
+    #
+    def write(process_response)
+      write = Write::Postgres.new(write_options, process_response)
+
+      write.execute
+    end
+
+    private
+
+    def unprocessable_response(read_data)
+      read_data.nil? || read_data == {} || read_data["domains_limits"] == [] || read_data["domain_wip_count"] == []
+    end
+
+    def exceedded_counts(limits, counts)
+      counts.to_a.map do |domain_wip_count|
+        domain, count = domain_wip_count
+        domain_limit = limits[domain]
+
+        { domain:, exceeded: count - domain_limit } if count > domain_limit
+      end.compact
+    end
+  end
+end
