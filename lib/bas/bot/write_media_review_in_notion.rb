f29e52be07c7a5f533b641949e148be2e3136c1d
FelipeGuzmanSierra <97761783+FelipeGuzmanSierra@users.noreply.github.com>
2024-06-27 12:47:57 -0500

    Add write media review in notion bot

---
+++ lib/bas/bot/write_media_review_in_notion.rb
@@ -0,0 +1,134 @@
+# frozen_string_literal: true
+
+require "json"
+
+require_relative "./base"
+require_relative "../read/default"
+require_relative "../utils/notion/request"
+require_relative "../write/postgres"
+
+module Bot
+  ##
+  # The Bot::WriteMediaReviewInNotion class serves as a bot implementation to read from a postgres
+  # shared storage formated notion blocks and send them to a Notion page
+  #
+  # <br>
+  # <b>Example</b>
+  #
+  #   options = {
+  #     read_options: {
+  #       connection: {
+  #         host: "localhost",
+  #         port: 5432,
+  #         dbname: "bas",
+  #         user: "postgres",
+  #         password: "postgres"
+  #       },
+  #       db_table: "review_media",
+  #       tag: "FormatMediaReview"
+  #     },
+  #     process_options: {
+  #       secret: "notion_secret"
+  #     },
+  #     write_options: {
+  #       connection: {
+  #         host: "localhost",
+  #         port: 5432,
+  #         dbname: "bas",
+  #         user: "postgres",
+  #         password: "postgres"
+  #       },
+  #       db_table: "review_media",
+  #       tag: "WriteMediaReviewInNotion"
+  #     }
+  #   }
+  #
+  #   bot = Bot::WriteMediaReviewInNotion.new(options)
+  #   bot.execute
+  #
+  class WriteMediaReviewInNotion < Bot::Base
+    CHUNK_SIZE = "1000"
+
+    # read function to execute the PostgresDB Read component
+    #
+    def read
+      reader = Read::Postgres.new(read_options.merge(conditions))
+
+      reader.execute
+    end
+
+    # process function to execute the Notion utility to send formated blocks to a page
+    #
+    def process
+      return { success: { review_added: nil } } if unprocessable_response
+
+      response = Utils::Notion::Request.execute(params)
+
+      if response.code == 200
+        { success: { page_id: read_response.data["page_id"], property: read_response.data["property"] } }
+      else
+        { error: { message: response.parsed_response, status_code: response.code } }
+      end
+    end
+
+    # write function to execute the PostgresDB write component
+    #
+    def write
+      write = Write::Postgres.new(write_options, process_response)
+
+      write.execute
+    end
+
+    private
+
+    def conditions
+      {
+        where: "archived=$1 AND tag=$2 AND stage=$3 ORDER BY inserted_at ASC",
+        params: [false, read_options[:tag], "unprocessed"]
+      }
+    end
+
+    def params
+      {
+        endpoint: "blocks/#{read_response.data["page_id"]}/children",
+        secret: process_options[:secret],
+        method: "patch",
+        body:
+      }
+    end
+
+    def body
+      { children: [{ object: "block", type: "toggle", toggle: }] }
+    end
+
+    def toggle
+      {
+        rich_text: [{ type: "text", text: { content: toggle_title } }, mention],
+        children: toggle_childrens
+      }
+    end
+
+    def toggle_childrens
+      childrens = JSON.parse(read_response.data["review"])
+
+      childrens["children"]
+    end
+
+    def mention
+      {
+        type: "mention",
+        mention: {
+          type: "user",
+          user: { id: read_response.data["created_by"] }
+        }
+      }
+    end
+
+    def toggle_title
+      case read_response.data["media_type"]
+      when "image" then "Image review results/"
+      when "paragraph" then "Text review results/"
+      end
+    end
+  end
+end
