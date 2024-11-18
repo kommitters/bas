d33a965e5577da5095b34404d3db7d5e728b8ca8
Eleazar Meza <eleazar@kommit.co>
2024-11-18 17:41:28 -0400

    Add require_relative to expose bot and shared storage classes

--- lib/bas.rb
+++ lib/bas.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 require_relative "bas/version"
+require_relative "bas/bot/base"
+require_relative "bas/shared_storage/base"
+require_relative "bas/shared_storage/default"
+require_relative "bas/shared_storage/postgres"
 
 module Bas
   class Error < StandardError; end
